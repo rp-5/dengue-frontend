9cc9dcf1314f361fd20177b262793c77a35dd75b
BhrunoB8 <bhruno.roan@yahoo.com>
2022-07-08 10:45:14 -0300

    Campos dos casos em humanos

--- dengue/lib/src/pages/human_edit.dart
+++ dengue/lib/src/pages/human_edit.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
-class HumanEdit extends StatelessWidget {
+class HumanEdit extends StatefulWidget {
+  @override
+  HumanEditState createState() => HumanEditState();
   const HumanEdit({Key? key}) : super(key: key);
+
+
+
+}
+
+class HumanEditState extends State<HumanEdit>{
+
+  List<String> sintomas = <String>['Tosse', 'Febre'];
+  String dropdownSintoma = 'Tosse';
+
+  List<String> gravidade = <String>['Baixa', 'Média', 'Alta'];
+  String dropdownGravidade = 'Baixa';
+
+  List<String> status = <String>['Sem Suspeita', 'Suspeito', 'Confirmado', 'Descartado'];
+  String dropdownStatus = 'Sem Suspeita';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: getBody());
   }
 
+
+  
+
   Widget getBody() {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Edição de Caso em Humano'),
+        title: const Text('Registro de Caso em Humano'),
         backgroundColor: Colors.black,
         actions: <Widget>[
           IconButton(
@@ -21,7 +41,70 @@ class HumanEdit extends StatelessWidget {
           ),
         ],
       ),
-      body: const Padding(padding: EdgeInsets.all(10)),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nome*'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Idade*'),
+                keyboardType: TextInputType.number
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Endereço*'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Complemento'),
+              ),
+              DropdownButton(value: dropdownSintoma, items: sintomas.map<DropdownMenuItem<String>>(
+                (String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    );
+                },
+              ).toList(), 
+              onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownSintoma = newValue!;
+                  });
+                },
+              ),
+              DropdownButton(value: dropdownGravidade, items: gravidade.map<DropdownMenuItem<String>>(
+                (String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    );
+                },
+              ).toList(), 
+              onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownGravidade = newValue!;
+                  });
+                },
+              ),
+              DropdownButton(value: dropdownStatus, items: status.map<DropdownMenuItem<String>>(
+                (String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    );
+                },
+              ).toList(), 
+              onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownStatus = newValue!;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- dengue/lib/src/pages/human_register.dart
+++ dengue/lib/src/pages/human_register.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
-class HumanRegister extends StatelessWidget {
+class HumanRegister extends StatefulWidget {
+  @override
+  HumanRegisterState createState() => HumanRegisterState();
   const HumanRegister({Key? key}) : super(key: key);
 
+  
+}
+
+class HumanRegisterState extends State<HumanRegister>{
+  List<String> sintomas = <String>['Tosse', 'Febre'];
+  String dropdownSintoma = 'Tosse';
+
+  List<String> gravidade = <String>['Baixa', 'Média', 'Alta'];
+  String dropdownGravidade = 'Baixa';
+
+  List<String> status = <String>['Sem Suspeita', 'Suspeito', 'Confirmado', 'Descartado'];
+  String dropdownStatus = 'Sem Suspeita';
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: getBody());
   }
 
+  
+
   Widget getBody() {
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Registro de Caso em Humano'),
@@ -27,10 +45,59 @@ class HumanRegister extends StatelessWidget {
           child: Column(
             children: <Widget>[
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Nome'),
+                decoration: const InputDecoration(labelText: 'Nome*'),
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Idade*'),
+                keyboardType: TextInputType.number
               ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Email'),
+                decoration: const InputDecoration(labelText: 'Endereço*'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Complemento'),
+              ),
+              DropdownButton(value: dropdownSintoma, items: sintomas.map<DropdownMenuItem<String>>(
+                (String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    );
+                },
+              ).toList(), 
+              onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownSintoma = newValue!;
+                  });
+                },
+              ),
+              DropdownButton(value: dropdownGravidade, items: gravidade.map<DropdownMenuItem<String>>(
+                (String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    );
+                },
+              ).toList(), 
+              onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownGravidade = newValue!;
+                  });
+                },
+              ),
+              DropdownButton(value: dropdownStatus, items: status.map<DropdownMenuItem<String>>(
+                (String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    );
+                },
+              ).toList(), 
+              onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownStatus = newValue!;
+                  });
+                },
               ),
             ],
           ),
@@ -38,4 +105,7 @@ class HumanRegister extends StatelessWidget {
       ),
     );
   }
+  
+
+  
 }
